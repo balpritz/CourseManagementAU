4735debe6c499d1d0b359658c569c6b8cc9a4c42
balpritz <balpreet@Balpreets-MacBook-Air.local>
2020-05-31 18:26:56 +0530

    added models for course and feedback databases

---
+++ backend/course-management-system/src/main/java/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.CourseDetails;
+
+public interface CourseRepository extends JpaRepository<CourseDetails, Integer> {
+
+}
---
+++ backend/course-management-system/src/main/java/repository/FeedbackRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.CourseFeedback;
+
+public interface FeedbackRepository extends JpaRepository<CourseFeedback, Integer> {
+
+}
